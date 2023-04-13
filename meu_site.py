6dee21adfa339b3d367b051cc286dc98db3e711a
Ravel Soares <ravelsoares64@gmail.com>
2023-04-13 13:04:40 -0300

    first commit

---
+++ meu_site.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+# route -> hashtagtreinamentos.com/
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def homepage():
+    return "homepage.html"
+
+@app.route("/contatos")
+def contatos():
+    return "contatos.html
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return "usuarios.html"
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    # servidor do heroku
+
